0461d6c209953ca799b1f74ecc32ad377a9fc1f6
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 09:14:59 -0400

    Replace magrittr pipe with base R

--- R/request.R
+++ R/request.R
@@ -6,6 +6,6 @@
 #' @export
 #'
 request <- function(...) {
-  httr2::request("https://en.wikipedia.org") %>%
+  httr2::request("https://en.wikipedia.org") |>
     httr2::req_url_path_append("/api/rest_v1")
 }
