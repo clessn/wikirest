edb8306b2c88b0beae31f69e710881b584a2556e
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 13:37:13 -0400

    Add test for `add_user_agent()` (#32)
    * Add tests for add_user_agent()
    
    * Increment version number to 0.0.0.9010

--- tests/testthat/test-request.R
+++ tests/testthat/test-request.R
@@ -4,3 +4,16 @@ test_that("create request works", {
   expect_equal(data[["url"]], "https://wikimedia.org/api/rest_v1")
   expect_s3_class(data, "httr2_request")
 })
+
+test_that("add user agent works", {
+  expect_error(add_user_agent())
+
+  req <- create_req() |>
+    add_user_agent()
+
+  user_agent <- req[["options"]][["useragent"]]
+
+  expect_equal(user_agent, "wikirest (https://github.com/clessn; info@clessn.ca)")
+
+  expect_s3_class(req, "httr2_request")
+})
