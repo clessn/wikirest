69de21d5e77bc954641db9495c38704a33569a55
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 10:45:58 -0400

    Create function

--- R/pageviews.R
+++ R/pageviews.R
@@ -9,3 +9,24 @@ create_pageviews_req <- function() {
   create_metrics_req() |>
     httr2::req_url_path_append("/pageviews")
 }
+
+#' Get most viewed articles per country
+#'
+#' @param country description
+#' @param access all-access, desktop, mobile-app, mobile-web
+#'
+#' @keywords internal
+#'
+#' @return A modified HTTP request.
+#' @export
+#'
+get_most_viewed_per_country <- function(country = "CA", access = "all-access", year = "2022", month = "01", day = "01") {
+
+  path <- paste(country, access, year, month, day, sep = "/")
+
+  wikirest::create_pageviews_req() |>
+    httr2::req_url_path_append("/top-per-country") |>
+    httr2::req_url_path_append(path) |>
+    httr2::req_throttle(100/1) |>
+    httr2::req_perform()
+}
