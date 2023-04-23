de43604a08ee719f2f13af609c63662ea34f2dba
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 09:42:07 -0400

    Remove ... parameter

--- R/metrics.R
+++ R/metrics.R
@@ -1,13 +1,11 @@
 #' Create metrics request
 #'
-#' @param ... further arguments to httr's GET.
-#'
 #' @keywords internal
 #'
 #' @return A modified HTTP request.
 #' @export
 #'
-create_metrics_req <- function(...) {
+create_metrics_req <- function() {
   request() |>
     httr2::req_url_path_append("/metrics")
 }
