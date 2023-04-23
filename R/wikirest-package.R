bb77b2564e92c48984e37a2567656d699e2bb279
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 12:08:29 -0400

    Add dependencies

--- R/wikirest-package.R
+++ R/wikirest-package.R
@@ -2,7 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr mutate
 #' @importFrom httr2 req_url_path_append
 #' @importFrom httr2 request
+#' @importFrom purrr map
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr unnest
 ## usethis namespace: end
 NULL
