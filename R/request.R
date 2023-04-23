781db37e7fc990df17818e77ef179851d0944f22
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 09:55:23 -0400

    Create function

--- R/request.R
+++ R/request.R
@@ -9,3 +9,17 @@ create_req <- function() {
   httr2::request("https://en.wikipedia.org") |>
     httr2::req_url_path_append("/api/rest_v1")
 }
+
+#' Add user agent to request
+#'
+#' @param req A HTTP request.
+#'
+#' @keywords internal
+#'
+#' @return A modified HTTP request.
+#' @export
+#'
+add_user_agent <- function(req) {
+  req |>
+    httr2::req_user_agent("wikirest (https://github.com/clessn; info@clessn.ca)")
+}
