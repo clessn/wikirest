989a21013790434dc5ba9f68fbce559eda722f00
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 09:18:20 -0400

    Create request function (#2)
    * Create function
    
    * Replace magrittr pipe with base R
    
    * Add httr dependencies
    
    * Add keywords internal
    
    * Increment version number to 0.0.0.9001

---
+++ R/request.R
@@ -0,0 +1,13 @@
+#' Create request
+#'
+#' @param ... further arguments to httr's GET.
+#'
+#' @keywords internal
+#'
+#' @return A modified HTTP request.
+#' @export
+#'
+request <- function(...) {
+  httr2::request("https://en.wikipedia.org") |>
+    httr2::req_url_path_append("/api/rest_v1")
+}
---
+++ R/wikirest-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom httr2 req_url_path_append
+#' @importFrom httr2 request
+## usethis namespace: end
+NULL
