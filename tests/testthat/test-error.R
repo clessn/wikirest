03aa8106e3d50211eb29ee5dd6d10f76f200aa26
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 12:38:57 -0400

    Add tests for error if parameter is null

---
+++ tests/testthat/test-error.R
@@ -0,0 +1,7 @@
+test_that("error if parameter null works", {
+  expect_error(error_if_parameter_null(parameters = list(NULL)))
+  expect_error(error_if_parameter_null(parameters = list("a", NULL, 2)))
+  expect_no_error(error_if_parameter_null(parameters = list("a", 2, TRUE, FALSE)))
+  expect_no_error(error_if_parameter_null(parameters = list(NA, NaN)))
+  #expect_no_error(error_if_parameter_null(parameters = list("NULL", "Null", "null")))
+})
