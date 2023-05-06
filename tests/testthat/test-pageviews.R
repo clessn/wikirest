222561cc3f5a1989e6a50d86699925ae56035ff0
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-05-06 13:00:37 -0400

    Adapt tests for deprecated arg

--- tests/testthat/test-pageviews.R
+++ tests/testthat/test-pageviews.R
@@ -7,6 +7,21 @@ test_that("create pageviews request works", {
 
 test_that("create pageviews request works", {
   data <- get_most_viewed_per_country(country = "CA", access = "all-access",
+                                      date = "2022-01-01",
+                                      tidy = FALSE)
+
+  expect_s3_class(data, "httr2_response")
+
+  data <- get_most_viewed_per_country(country = "CA", access = "all-access",
+                                      date = "2022-01-01",
+                                      tidy = TRUE)
+
+  expect_s3_class(data, "tbl_df")
+
+  # Deprecated arguments
+  withr::local_options(lifecycle_verbosity = "quiet")
+
+  data <- get_most_viewed_per_country(country = "CA", access = "all-access",
                                       year = "2022", month = "01", day = "01",
                                       tidy = FALSE)
 
@@ -18,9 +33,4 @@ test_that("create pageviews request works", {
 
   expect_s3_class(data, "tbl_df")
 
-  withr::local_options(lifecycle_verbosity = "quiet")
-
-  lifecycle::expect_deprecated(get_most_viewed_per_country(country = "CA", access = "all-access",
-                                                           year = "2022", month = "01", day = "01",
-                                                           tidy = TRUE))
 })
